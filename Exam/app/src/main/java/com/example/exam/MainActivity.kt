fef092528a09efb83c20e16e12618f7869613f5c
Muhammad Hussain <128447077+Hussainkhan777@users.noreply.github.com>
2024-03-27 15:51:08 +0500

    3rd

--- Exam/app/src/main/java/com/example/exam/MainActivity.kt
+++ Exam/app/src/main/java/com/example/exam/MainActivity.kt
@@ -16,58 +16,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ExamTheme {
-//              stateManagement()
-//                MainScreen()
-//                Prac_Screen()
-//                CameraPermission()
-//                MakeCallAndBrowseApp()
-//                MyComposabIeFunction()
-//                LogInSignUpNavigation()
+
                 ActorsNavigation()
 
             }
         }
     }
 
-    /*This below Code is only for make call and browse app functionality
-
-
-    @Deprecated("Deprecated in Java")
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CALL_PHONE_PERMISSION) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted, make the call
-                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
-            } else {
-                // Permission denied, show a message or handle it accordingly
-                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-    */
 }
-const val REQUEST_CALL_PHONE_PERMISSION = 123
 
-/*
-@Composable
-fun LogInSignUpNavigation() {
-    val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "login") {
-        composable("login") {
-            LoginPage(navController)
-        }
-        composable("signup") {
-            SignupPage(navController)
-        }
-    }
-}
-*/
 
 
 @Composable
@@ -99,13 +57,7 @@ fun ActorsNavigation() {
 @Composable
 fun GreetingPreview() {
     ExamTheme {
-//        stateManagement()
-//        MainScreen()
-//        Prac_Screen()
-//        CameraPermission()
-//        MakeCallAndBrowseApp()
-//        MyComposabIeFunction()
-//        LogInSignUpNavigation()
+
         ActorsNavigation()
     }
 }
